132b74cc8bc87780d330b11cc630a1a4ffb38484
Ayoola Olaoye <communicatti@gmail.com>
2023-06-22 17:07:52 +0100

    Set up Main, working on rows

---
+++ src/Requests.js
@@ -0,0 +1,11 @@
+const key = 'ab8bbae9b4bb21ebf26f3e1d13375105'
+
+const requests = {
+    requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`,
+    requestTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=1`,
+    requestTrending: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=2`,
+    requestHorror: `https://api.themoviedb.org/3/search/movie?api_key=${key}&language=en-US&query=horror&page=1&include_adult=false`,
+    requestUpcoming: `https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=en-US&page=1`,
+  };
+
+  export default requests
---
+++ src/components/New.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const New = () => {
+  return (
+    <div><h1 className='text-white'>TestH1</h1>></div>
+  )
+}
+
+export default New
